5cdb8705fda608349ba52360d1d01b4fbe137a8e
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-04 17:15:54 -0500

    update admin display

--- ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/admin.py
+++ ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/admin.py
@@ -5,7 +5,7 @@ from models import Person
 
 class PersonAdmin(ModelAdmin):
     fieldsets = ('Name', dict(fields=('first_name', 'last_name', 'gender'))),
-    list_display = 'first_name', 'last_name', 'gender'
+    list_display = 'last_name', 'first_name', 'gender'
     search_fields = 'first_name', 'last_name'
 
 site.register(Person, PersonAdmin)
