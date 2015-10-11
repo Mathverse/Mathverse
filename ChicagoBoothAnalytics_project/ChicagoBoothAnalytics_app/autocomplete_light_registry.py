baf78747144bb95dd085e86d1c4706373fe1ccab
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-10 21:55:49 -0500

    enable autocomplete for Role & GeogRegion

--- ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/autocomplete_light_registry.py
+++ ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/autocomplete_light_registry.py
@@ -1,7 +1,8 @@
 from autocomplete_light import register
-from models import Person, Org, FactType
+from models import Role, GeogRegion, Person, Org, FactType
 
-
-register(Person, search_fields=('first_name', 'first_name_alias', 'last_name'))
+register(Role, search_fields=('title',))
+register(GeogRegion, search_fields=('name',))
 register(Org, search_fields=('name',))
+register(Person, search_fields=('first_name', 'first_name_alias', 'last_name'))
 register(FactType, search_fields=('label',))
