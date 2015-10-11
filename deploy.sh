5ae1d0196d44b03f67ec23cb98301fbac439d210
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-10 20:51:16 -0500

    m

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ python ChicagoBoothAnalytics_project/manage.py migrate
 python ChicagoBoothAnalytics_project/manage.py collectstatic
 
 git add --all
-git commit -m "deploy changes to AWS EB Django app"
+
+getopts m: git_commit_message
+git commit -m "$git_commit_message"
+
 git push --all
 
 eb deploy --no-verify-ssl
