7feb336844a6abd68a6aaf48c4c2d827580c252d
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-11-13 10:18:35 -0600

    add autocomplete for User

--- ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/autocomplete_light_registry.py
+++ ChicagoBoothAnalytics_project/ChicagoBoothAnalytics_app/autocomplete_light_registry.py
@@ -2,7 +2,7 @@ from autocomplete_light import register
 from django.contrib.auth.models import User
 from models import BusinessSector, Role, GeogRegion, Person, Org, FactType
 
-register(User, search_fields=('username', 'first_name', 'last_name', 'email_address'))
+register(User, search_fields=('username', 'first_name', 'last_name', 'email'))
 register(BusinessSector, search_fields=('name',))
 register(Role, search_fields=('title',))
 register(GeogRegion, search_fields=('name',))
