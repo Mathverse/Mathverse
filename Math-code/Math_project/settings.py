12b14783caa53423d78376280d5183192ab95fbb
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2017-05-29 15:38:42 +0700

    add AWS RDS PostgreSQL DB details

--- Math-code/Math_project/settings.py
+++ Math-code/Math_project/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'Math_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MathDB',
+        'USER': 'MinhHa27255',
+        'PASSWORD': 'MinhHa27255',
+        'HOST': 'math.c6b0ebeid6nw.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
