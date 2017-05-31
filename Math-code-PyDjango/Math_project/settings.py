80514195b4b910da77f925a618984f0e51ccda18
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2017-05-31 19:06:28 +0900

    Revert "set STATIC_URL = STATIC_ROOT + '/'"
    This reverts commit 748b8a83c0dfd36ae53e53e59acaf7461ea976d2.

--- Math-code-PyDjango/Math_project/settings.py
+++ Math-code-PyDjango/Math_project/settings.py
@@ -149,4 +149,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = STATIC_ROOT + '/'
+STATIC_URL = '/static/'
