cdb706bbb6d8f7b0e7bf384e824ca9e48b058664
MBALearnsToCode <MBALearnsToCode@UChicago.edu>
2017-06-05 10:42:39 -0700

    enable Grappelli

--- Math-code-PyDjango/Math_project/settings.py
+++ Math-code-PyDjango/Math_project/settings.py
@@ -93,6 +93,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # Grappelli
+                'django.template.context_processors.request'
             ],
         },
     },
